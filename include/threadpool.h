0d73bb64fd958bc84485d80578a38dc2685e443c
kanishka <pujarakanishka@gmail.com>
2026-07-07 22:05:58 +0530

    Add condition variable based worker shutdown

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,11 +1,14 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <mutex>
 
 class ThreadPool{
     private:
+        std::mutex mtx;
         std::atomic<bool> stop;
         std::vector<std::thread> threadsVec;
+        std::condition_variable cv;
         void workerLoop();
     public:
         ThreadPool(int count);
